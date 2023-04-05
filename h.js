4256004d8135a936aa932ff854fff0bcc211ce75
anoneer <125512713+anoneer@users.noreply.github.com>
2023-04-05 18:04:13 -0300

    Add files via upload

---
+++ h.js
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Como a Logística pode Fazer a Diferença na Satisfação do Cliente</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f5f5f5;
+		}
+		.container {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #fff;
+			border: 1px solid #ccc;
+			box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+		}
+		h1 {
+			font-size: 28px;
+			margin-bottom: 20px;
+			color: #333;
+			text-align: center;
+		}
+		h2 {
+			font-size: 22px;
+			margin-bottom: 10px;
+			color: #333;
+		}
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+		li {
+			margin-bottom: 20px;
+			display: flex;
+			align-items: center;
+		}
+		li img {
+			width: 100px;
+			margin-right: 20px;
+		}
+		li p {
+			font-size: 18px;
+			margin: 0;
+			color: #666;
+			line-height: 1.5;
+		}
+		.button {
+			display: inline-block;
+			background-color: #f0ad4e;
+			color: #fff;
+			padding: 10px 20px;
+			border-radius: 5px;
+			text-decoration: none;
+			margin-top: 20px;
+			transition: all 0.3s ease;
+		}
+		.button:hover {
+			background-color: #d9534f;
+			cursor: pointer;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>Como a Logística pode Fazer a Diferença na Satisfação do Cliente</h1>
+		<ul>
+			<li>
+				<img src="https://cdn.pixabay.com/photo/2016/03/31/19/58/box-1299549_960_720.png" alt="Entrega no Prazo">
+				<div>
+					<h2>Entrega no Prazo</h2>
+					<p>Com uma logística eficiente, seus produtos chegam no prazo certo, evitando a insatisfação do cliente.</p>
+				</div>
+			</li>
+			<li>
+				<img src="https://cdn.pixabay.com/photo/2013/07/12/14/06/package-147374_960_720.png" alt="Produtos em Perfeitas Condições">
+				<div>
+					<h2>Produtos em Perfeitas Condições</h2>
+					<p>A logística eficiente garante que seus produtos cheguem em perfeitas condições, evitando problemas como produtos danificados ou extraviados.</p>
+				</div>
+			</li>
+			<li>
+				<img src="https://cdn.pixabay.com/photo/2016/11/08/05/07/warehouse-1805959_960_720.png" alt="Redução de Custos">
+				<div>
+					<h2>Red}
+
